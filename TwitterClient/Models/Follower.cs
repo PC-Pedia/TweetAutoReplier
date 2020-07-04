c53075c4244ba4b81ca3b3c29849c6981d1c07ae
Jonathan D <jdahan91@users.noreply.github.com>
2020-07-04 10:36:23 +0300

    Update Follower.cs

--- TwitterClient/Models/Follower.cs
+++ TwitterClient/Models/Follower.cs
@@ -1,14 +1,49 @@
 ﻿using System.Collections.Generic;
+using TwitterClient.ViewModels;
 
 namespace TwitterClient.Models
 {
-    public class Follower
+    public class Follower : BaseViewModel
     {
-        public string ScreenName { get; set; }
-        public string IdStr { get; set; }
-        public string NoOfReplies { get; set; }
-        public string Filter { get; set; }
-        public string DisplayTime { get; set; }
+        private string _screenName;
+        public string ScreenName
+        {
+            get { return _screenName; }
+            set { _screenName = value; RaisePropChanged(); }
+        }
+
+
+        private string _idStr;
+        public string IdStr
+        {
+            get { return _idStr; }
+            set { _idStr = value; RaisePropChanged(); }
+        }
+
+
+        private string _noOfReplies;
+        public string NoOfReplies
+        {
+            get { return _noOfReplies; }
+            set { _noOfReplies = value; RaisePropChanged(); }
+        }
+
+
+        private string _filter;
+        public string Filter
+        {
+            get { return _filter; }
+            set { _filter = value; RaisePropChanged(); }
+        }
+
+
+        private string _displayTime;
+        public string DisplayTime
+        {
+            get { return _displayTime; }
+            set { _displayTime = value; RaisePropChanged(); }
+        }
+
         public List<string> Messages { get; set; }
         public Follower()
         {
